d08e6c079ae7f3f6c1a8554cb0fd099fd0bdcc59
Aioshiro <aioshiro@hotmail.fr>
2021-03-01 15:55:15 +0100

    Added Dynamite model and fixed the sound when grabbed

---
+++ Assets/Scripts/WhenGrabbed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class WhenGrabbed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    abstract public void Grab();
+
+    abstract public void Released();
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
