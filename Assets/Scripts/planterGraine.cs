80d13efb3fb498c53a21c64d5aad5b54e77ce9f9
Aioshiro <aioshiro@hotmail.fr>
2021-03-16 14:50:43 +0100

    first script to plant seed

---
+++ Assets/Scripts/planterGraine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planterGraine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Graine")
+        {
+            other.gameObject.transform.SetParent(this.transform);
+            other.gameObject.transform.localPosition = Vector3.zero;
+            Destroy(other.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+}
